00ff031e1e1463b9f33ab08ec764d37cf45d7147
MHC1 <thiagofelipe.tel@gmail.com>
2022-04-30 18:09:10 -0300

    update_vlans

---
+++ lab22_script.py
@@ -0,0 +1,27 @@
+import getpass
+import sys
+import telnetlib
+
+user = input("Entre com seu usuário: ")
+password = getpass.getpass()
+IP_Switches = open("IP_Switches")
+
+for EachSwitche in IP_Switches:
+    print("Configurando Switch " + str(EachSwitche) + "\n")
+    tn = telnetlib.Telnet(EachSwitche)
+    tn.read_until(b"Username: ")
+    tn.write(user.encode("ascii") + b"\n")
+    if password:
+        tn.read_until(b"Password: ")
+        tn.write(password.encode("ascii") + b"\n")
+
+    tn.write(b"config t\n")
+
+    for n_vlans in range(2,4):
+        tn.write("vlan " + str(n_vlans) + "\n")
+        tn.write("name Phyton_VLAN_" + str(n_vlans) + "\n")
+
+    tn.write(b"end\n")
+    tn.write(b"exit\n")
+
+    print(tn.read_all().decode("ascii"))
